a404903d960eb60d55ce7c0503bceacc455fd432
Roberto Requena <rart3001@gmail.com>
2015-07-24 18:34:33 -0430

    Merge remote-tracking branch 'newbranch/master'

---
+++ fermat-api/src/main/java/com/bitdubai/fermat_api/layer/all_definition/navigation_structure/interfaces/FermatScreenSwapper.java
@@ -0,0 +1,13 @@
+package com.bitdubai.fermat_api.layer.all_definition.navigation_structure.interfaces;
+
+/**
+ * Created by Furszyfer Matias on 2015.07.23..
+ */
+public interface FermatScreenSwapper {
+
+    public void changeScreen();
+
+    public void setScreen(String screen);
+
+    public void setParams(Object[] objects);
+}
